5efd40ffe9faee05db7ede4cae92dd69fc47892d
Julia Conzon <juliaconzon@gmail.com>
2017-03-19 17:43:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   //res.render('error.jade')
-  //res.send("Sorry can't find that!")
+  res.send("Sorry can't find that!")
   // render the error page
   res.status(err.status || 500);
 });
