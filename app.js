96a1f8b6b88c496a895ecac6a05d9d54e3096348
Julia Conzon <juliaconzon@gmail.com>
2017-03-19 18:09:09 -0400

    removed error handler

--- app.js
+++ app.js
@@ -49,22 +49,22 @@ app.get('/get', function(req, res){
  	res.send(val);
 });
 
-// catch 404 and forward to error handler. 404 error indicates the app ran out of options
+/* catch 404 and forward to error handler. 404 error indicates the app ran out of options
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+ error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   //res.render('error.jade')
-  res.send("Sorry can't find that!")
+  //res.send("Sorry can't find that!")
   // render the error page
   res.status(err.status || 500);
-});
+});*/
 
 module.exports = app;
