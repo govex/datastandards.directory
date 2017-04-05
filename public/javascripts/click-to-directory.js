f1c1ba1b877387a4ae5c18a20f8309ddca1dfbe0
Noznoc <juliaconzon@gmail.com>
2017-04-05 19:11:43 -0400

    Fixed text and hover event

--- public/javascripts/click-to-directory.js
+++ public/javascripts/click-to-directory.js
@@ -1,5 +1,6 @@
 // this function runs when a client clicks on the .square class (in other words, the categories on index.html or directory.html)
 $(function(){
+	
 	var category = "", // store category variable
 		selectedStandard = []; // array for selected standards
 
@@ -13,27 +14,9 @@ $(function(){
 		category = $(this).html(); // store the category name
 		selectedCategory = $(this).attr('value'); // store the value of the category button (i.e., the category name)
 		if(event.type == "mouseover"){ // if the user hover overs the category button show the definitions
-			if (selectedCategory == "Annual Budget"){
-				$(this).hover(function () {$(this).html("Municipal budgets that plan for expenditure");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Building Permits") {
-				$(this).hover(function () {$(this).html("Municial permits for construction");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Crime Statistics") {
-				$(this).hover(function () {$(this).html("Municipal crime data");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Election Results") {
-				$(this).hover(function () {$(this).html("Municipal major election results by consitutency/district");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Expenditure") {
-				$(this).hover(function () {$(this).html("Municipal records of past spending");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Public Facilities") {
-				$(this).hover(function () {$(this).html("Municipal public facilities, such as schools, parks, and daycare, and their location information");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Real-Time Transit") {
-				$(this).hover(function () {$(this).html("Municipal or commissioned transit services' (e.g., buses, subway) real-time information, such as the location of a bus in real-time");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Road Construction") {
-				$(this).hover(function () {$(this).html("Municipal current and planned road construction");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Service Requests") {
-				$(this).hover(function () {$(this).html("Non-emergency service requests to a municipality, such as a request for graffiti removal");}, function () {$(this).html(category);}); // provide a description of the category 
-			} else if (selectedCategory == "Zoning") {
-				$(this).hover(function () {$(this).html("Municipal specifications on what development is allowed in a given piece of land");}, function () {$(this).html(category);}); // provide a description of the category 
-			} 
+			var categoryIcon = $(this).find(".category-icon"); // get the category's icon
+			var definition = $(this).find(".definition"); // get the category's definition
+			$(this).hover(function () {categoryIcon.hide(); definition.show();}, function () {categoryIcon.show(); definition.hide();}); // toggle between showing the icon and the category  
 	    } else if(event.type == "click"){ // else if the user clicks on the category button
 			buildDirectory(selectedCategory); // function that will output all the standards associated with the selected category into the directory 
 	    }
