cbbee2069ae44e1d9d4f649e815e8e49d226fe10
icalouse <icalouse@yahoo.com>
2018-01-22 20:20:44 -0500

    fixed multiple subcategories view fixed date format

--- public/javascripts/timestamp.js
+++ public/javascripts/timestamp.js
@@ -1,6 +1,7 @@
 function timestamp() {
 	var recorded = document.getElementById("timestamp").value // create variable to store the date
 	var date = new Date(); // get the current date
-	recorded = sub(date.toISOString(), 10);
+	date = date.toISOString();
+	recorded = date.substring(0, 10);
 	document.getElementById("timestamp").value = recorded; // update the timestamp to this reformatted date
 }
